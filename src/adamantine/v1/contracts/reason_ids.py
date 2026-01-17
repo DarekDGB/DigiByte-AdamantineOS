bf564e2e81ba89945cbb1edbd70b1eaa09b2bcf8
DarekDGB <majescyk@gmail.com>
2026-01-17 09:48:26 +0000

    chore: replace todo placeholders in v1 import path

--- src/adamantine/v1/contracts/reason_ids.py
+++ src/adamantine/v1/contracts/reason_ids.py
@@ -1,1 +1,8 @@
-todo
+from __future__ import annotations
+from enum import Enum
+
+
+class ReasonId(str, Enum):
+    TVA_MISSING_CONTEXT = "TVA_MISSING_CONTEXT"
+    TVA_MISSING_VERDICT = "TVA_MISSING_VERDICT"
+    TVA_VERDICT_NOT_ALLOW = "TVA_VERDICT_NOT_ALLOW"
