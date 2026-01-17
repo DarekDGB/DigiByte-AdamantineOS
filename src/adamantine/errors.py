a624b9a1d9bf796269bcf020303e7b5dc6287b8c
DarekDGB <majescyk@gmail.com>
2026-01-17 09:46:48 +0000

    chore: replace todo placeholders in v1 import path

--- src/adamantine/errors.py
+++ src/adamantine/errors.py
@@ -1,1 +1,9 @@
-todo
+from __future__ import annotations
+
+
+class AdamantineError(Exception):
+    """Base error for Adamantine Wallet OS."""
+
+
+class TVAError(AdamantineError):
+    """Raised when Truth Vector Authority denies continuation."""
