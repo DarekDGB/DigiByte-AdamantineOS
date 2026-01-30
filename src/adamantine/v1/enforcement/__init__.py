e937e6dc78a77ba16c62b396d2ac421dccffbcfb
DarekDGB <majescyk@gmail.com>
2026-01-30 07:40:26 +0000

    chore(enforcement): export nonce store selector

--- src/adamantine/v1/enforcement/__init__.py
+++ src/adamantine/v1/enforcement/__init__.py
@@ -1,4 +1,11 @@
 from .nonce_store import DurableNonceStore, InMemoryNonceStore, NonceStore
+from .nonce_store_selector import select_nonce_store
 from .tva_gate import enforce_tva
 
-__all__ = ["enforce_tva", "NonceStore", "DurableNonceStore", "InMemoryNonceStore"]
+__all__ = [
+    "enforce_tva",
+    "NonceStore",
+    "DurableNonceStore",
+    "InMemoryNonceStore",
+    "select_nonce_store",
+]
