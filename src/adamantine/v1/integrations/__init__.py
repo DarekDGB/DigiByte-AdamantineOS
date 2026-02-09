0bf76286442ec7a9ac7dff24d9c0af9942e08331
DarekDGB <majescyk@gmail.com>
2026-02-09 20:04:52 +0000

    export parse_shield_bundle_v3 in integrations package

--- src/adamantine/v1/integrations/__init__.py
+++ src/adamantine/v1/integrations/__init__.py
@@ -3,5 +3,6 @@ from __future__ import annotations
 from adamantine.v1.integrations.errors import AdapterError
 from adamantine.v1.integrations.qid_adapter import parse_qid_session
 from adamantine.v1.integrations.adaptive_core_adapter import parse_risk_report
+from adamantine.v1.integrations.shield_v3_adapter import parse_shield_bundle_v3
 
-__all__ = ["AdapterError", "parse_qid_session", "parse_risk_report"]
+__all__ = ["AdapterError", "parse_qid_session", "parse_risk_report", "parse_shield_bundle_v3"]
