238aafd4c6def44b88784e0ffbe12f275709db45
DarekDGB <majescyk@gmail.com>
2026-02-09 19:38:17 +0000

    add runtime package exports (adapter skeleton)

---
+++ src/adamantine/v1/runtime/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from .runtime_adapter import (
+    AdamantineRuntimeAdapter,
+    RuntimeClock,
+    RuntimeExecutorProvider,
+    RuntimeNonceStoreProvider,
+    RuntimePolicyProvider,
+    RuntimeServices,
+)
+
+__all__ = [
+    "AdamantineRuntimeAdapter",
+    "RuntimeClock",
+    "RuntimeExecutorProvider",
+    "RuntimeNonceStoreProvider",
+    "RuntimePolicyProvider",
+    "RuntimeServices",
+]
