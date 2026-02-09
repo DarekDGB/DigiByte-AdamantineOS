2e3d7ae2aac1486ca69037fe1ccd45136f73dfe2
DarekDGB <majescyk@gmail.com>
2026-02-09 20:18:51 +0000

    export parse_adaptive_core_oracle_v3 (oracle adapter entrypoint)

--- src/adamantine/v1/integrations/__init__.py
+++ src/adamantine/v1/integrations/__init__.py
@@ -1,8 +1,15 @@
 from __future__ import annotations
 
+from adamantine.v1.integrations.adaptive_core_adapter import parse_risk_report
+from adamantine.v1.integrations.adaptive_core_oracle_v3_adapter import parse_adaptive_core_oracle_v3
 from adamantine.v1.integrations.errors import AdapterError
 from adamantine.v1.integrations.qid_adapter import parse_qid_session
-from adamantine.v1.integrations.adaptive_core_adapter import parse_risk_report
 from adamantine.v1.integrations.shield_v3_adapter import parse_shield_bundle_v3
 
-__all__ = ["AdapterError", "parse_qid_session", "parse_risk_report", "parse_shield_bundle_v3"]
+__all__ = [
+    "AdapterError",
+    "parse_qid_session",
+    "parse_risk_report",
+    "parse_adaptive_core_oracle_v3",
+    "parse_shield_bundle_v3",
+]
