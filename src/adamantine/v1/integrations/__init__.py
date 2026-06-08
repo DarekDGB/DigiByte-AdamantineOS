f75c5280143904b8a0aecde04526e3560c169c0c
DarekDGB <majescyk@gmail.com>
2026-06-08 21:21:07 +0100

    Add ShieldV3AdapterHarness imports and exports

--- src/adamantine/v1/integrations/__init__.py
+++ src/adamantine/v1/integrations/__init__.py
@@ -5,6 +5,11 @@ from adamantine.v1.integrations.adaptive_core_oracle_v3_adapter import parse_ada
 from adamantine.v1.integrations.errors import AdapterError
 from adamantine.v1.integrations.qid_adapter import parse_qid_session
 from adamantine.v1.integrations.shield_v3_adapter import parse_shield_bundle_v3
+from adamantine.v1.integrations.shield_v3_adapter_harness import (
+    ShieldV3AdapterHarnessResult,
+    ShieldV3AdapterHarnessState,
+    run_shield_v3_adapter_harness,
+)
 from adamantine.v1.integrations.shield_orchestrator_receipt_verifier import (
     ShieldReceiptVerificationResult,
     ShieldReceiptVerificationState,
@@ -22,6 +27,9 @@ __all__ = [
     "parse_risk_report",
     "parse_adaptive_core_oracle_v3",
     "parse_shield_bundle_v3",
+    "run_shield_v3_adapter_harness",
+    "ShieldV3AdapterHarnessState",
+    "ShieldV3AdapterHarnessResult",
     "adapt_shield_orchestrator_receipt",
     "ShieldReceiptAdapterState",
     "ShieldReceiptAdapterResult",
