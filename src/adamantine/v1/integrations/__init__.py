34ea77c00cc4cd059dabcd17844023de5fbd4672
DarekDGB <majescyk@gmail.com>
2026-06-08 15:12:23 +0100

    Add ShieldReceiptAdapter imports to __init__.py

--- src/adamantine/v1/integrations/__init__.py
+++ src/adamantine/v1/integrations/__init__.py
@@ -5,6 +5,11 @@ from adamantine.v1.integrations.adaptive_core_oracle_v3_adapter import parse_ada
 from adamantine.v1.integrations.errors import AdapterError
 from adamantine.v1.integrations.qid_adapter import parse_qid_session
 from adamantine.v1.integrations.shield_v3_adapter import parse_shield_bundle_v3
+from adamantine.v1.integrations.shield_orchestrator_receipt_adapter import (
+    ShieldReceiptAdapterResult,
+    ShieldReceiptAdapterState,
+    adapt_shield_orchestrator_receipt,
+)
 
 __all__ = [
     "AdapterError",
@@ -12,4 +17,7 @@ __all__ = [
     "parse_risk_report",
     "parse_adaptive_core_oracle_v3",
     "parse_shield_bundle_v3",
+    "adapt_shield_orchestrator_receipt",
+    "ShieldReceiptAdapterState",
+    "ShieldReceiptAdapterResult",
 ]
