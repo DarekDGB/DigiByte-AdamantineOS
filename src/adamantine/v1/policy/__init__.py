fe0baa6b148ab0de4754c6aedcc7a8506585fc01
DarekDGB <majescyk@gmail.com>
2026-06-09 13:17:15 +0100

    Update __all__ to include final policy engine exports

--- src/adamantine/v1/policy/__init__.py
+++ src/adamantine/v1/policy/__init__.py
@@ -1,5 +1,17 @@
 from __future__ import annotations
 
+from adamantine.v1.policy.final_policy_engine import (
+    FinalPolicyEngineResult,
+    FinalPolicyEngineState,
+    LocalPolicyGateResult,
+    evaluate_final_policy_engine,
+)
 from adamantine.v1.policy.risk_policy import RiskPolicy
 
-__all__ = ["RiskPolicy"]
+__all__ = [
+    "FinalPolicyEngineResult",
+    "FinalPolicyEngineState",
+    "LocalPolicyGateResult",
+    "RiskPolicy",
+    "evaluate_final_policy_engine",
+]
