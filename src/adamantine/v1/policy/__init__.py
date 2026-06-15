29fd1cc9d01ab797ed2202ae6f174850083ca654
DarekDGB <majescyk@gmail.com>
2026-06-15 20:48:53 +0100

    Add ShieldRuntimeBoundary to policy exports

--- src/adamantine/v1/policy/__init__.py
+++ src/adamantine/v1/policy/__init__.py
@@ -6,12 +6,13 @@ from adamantine.v1.policy.final_policy_engine import (
     LocalPolicyGateResult,
     evaluate_final_policy_engine,
 )
-from adamantine.v1.policy.risk_policy import RiskPolicy
+from adamantine.v1.policy.risk_policy import RiskPolicy, ShieldRuntimeBoundary
 
 __all__ = [
     "FinalPolicyEngineResult",
     "FinalPolicyEngineState",
     "LocalPolicyGateResult",
     "RiskPolicy",
+    "ShieldRuntimeBoundary",
     "evaluate_final_policy_engine",
 ]
